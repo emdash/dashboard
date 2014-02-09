07d51ba56bdaada86add70a50949fb7c5d13af4d
Brandon Lewis <dotsony@gmail.com>
2014-02-09 00:23:58 -0800

    more comments

--- dashboard.ino
+++ dashboard.ino
@@ -52,6 +52,8 @@ char curchar;
 unsigned long nchars;
 
 
+// Prints an unsigned integer to the LCD in M:SS.mS format. For some
+// reason, the compiler didn't like time declared as a Time.
 void printTime(unsigned long time)
 {
   int minutes = (time / MINUTES) % 10;
@@ -72,6 +74,8 @@ void printTime(unsigned long time)
   lcd.print(buffer);
 }
 
+
+// Implements the lap time "page" of the display.
 void lapTimes (void)
 {
   float offset;
@@ -104,6 +108,7 @@ void lapTimes (void)
   lcd.print(wheel_speed);
 }
 
+// Impements the engine temperature page of the display
 void engineTemps(void)
 {
   lcd.setCursor(0, 0);
@@ -114,6 +119,7 @@ void engineTemps(void)
   lcd.print(water_temp);
 }
 
+// Currently just one sensor value, but that might change.
 void engineMisc(void)
 {
   lcd.setCursor(0, 0);
@@ -121,6 +127,8 @@ void engineMisc(void)
   lcd.print(oil_pressure);
 }
 
+// Random stuff for debugging purposes. Mostly not useful unless you
+// slow down the data stream.
 void debug(void)
 {
   lcd.clear();
@@ -141,39 +149,10 @@ void debug(void)
   lcd.print(incoming.f);
 }
 
-void handleChar(char sc)
-{
-  unsigned char c = (unsigned char) sc;
-  curchar = c;
-
-  switch (nchars++) {
-  case 0:
-    incoming.u = c & 0x0F;
-    return;
-  case 1:
-  case 2:
-  case 3:
-  case 4:
-    incoming.u <<= 7;
-    incoming.u |= c & 0x7f;
-    return;
-  case 5:
-    break;
-  default:
-    nchars = 0;
-    return;
-  }
-
-#if 0
-  Serial.print(" ");
-  Serial.print(String(incoming.u, HEX));
-  Serial.print(" "); 
-  Serial.print(incoming.f);
-  Serial.print(" '");
-  Serial.print(sc);
-  Serial.println("'");
-#endif
-  
+// Copies the current input to the appropriate sensor variable. Think
+// of it as a mapping between channel ID, variable, and data type.
+void updateValues(unsigned char c)
+{ 
   switch (c) {
   case 'k':
     lap = incoming.u;
@@ -202,6 +181,45 @@ void handleChar(char sc)
   }
 }
 
+// Protocol state machine. Called once for each byte received over
+// serial. Data is encoded such that null bytes never occur within
+// the message body, leaving allowing them to serve as delimiters.
+// Think of it as "base 128" as opposed to "base 64".
+void handleChar(char sc)
+{
+  unsigned char c = (unsigned char) sc;
+  curchar = c;
+
+  switch (nchars++) {
+  case 0:
+    incoming.u = c & 0x0F;
+    return;
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+    incoming.u <<= 7;
+    incoming.u |= c & 0x7f;
+    return;
+  case 5:
+    updateValues(c);
+    return;
+  default:
+    nchars = 0;
+    return;
+  }
+
+#if 0
+  Serial.print(" ");
+  Serial.print(String(incoming.u, HEX));
+  Serial.print(" "); 
+  Serial.print(incoming.f);
+  Serial.print(" '");
+  Serial.print(sc);
+  Serial.println("'");
+#endif
+} 
+
 void setup()
 {
   pinMode(backLight, OUTPUT);
@@ -216,7 +234,7 @@ void setup()
 
 void loop()
 {
-  int newPage = 4 * analogRead(page_knob) / 1024;
+  int newPage = 4 * analogRead(sensorPin) / 1024;
     
   if (millis() > (long(curFrame) * long(refreshInterval))) {
     update = true;
